d5ba0f2b9f1ce646ed46c76b3dd715e32f4faf1d
Clara Utsch <22400109@aluno.cotemig.com.br>
2026-06-16 11:29:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
+import os
+
 from flask import Flask
 
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def questao1():
-    return render_template("questao1.html", name = "Clara")
-
-@app.route("/questao2")
-def questao2():
-    dados = [{"nome": "Clara", "idade": 18}]
-    return render_template("questao2.html", alunos = dados)
+from controllers import calculadora_bp
+from models import Operacao, db
 
 
+def criar_app():
+    app = Flask(
+        __name__,
+        template_folder="views/templates",
+        static_folder="views/static",
+    )
+
+    pasta_aula = os.path.abspath(os.path.dirname(__file__))
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        pasta_aula, "calculadora.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    app.register_blueprint(calculadora_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+app = criar_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
